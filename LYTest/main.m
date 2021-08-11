f0ebf533ece80472ff7a20a883f9995b8f1a4e81
MKTech01 <mktech01@MKTech01deMini.lan>
2021-08-11 11:11:48 +0800

    objc4-818.2源码编译运行配置环境

---
+++ LYTest/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  LYTest
+//
+//  Created by MKTech01 on 2021/8/11.
+//
+
+#import <Foundation/Foundation.h>
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
